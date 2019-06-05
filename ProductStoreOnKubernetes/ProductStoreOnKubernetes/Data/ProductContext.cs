a7cc4b9d29c2473de2581c2893570bf016107a06
shishir <shishirroy1982@gmail.com>
2019-06-06 00:27:41 +0530

    draft version commiting

---
+++ ProductStoreOnKubernetes/ProductStoreOnKubernetes/Data/ProductContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductStoreOnKubernetes.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext (DbContextOptions<ProductContext> options)
+            : base(options)
+
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<ProductStoreOnKubernetes.Models.Product> Product { get; set; }
+    }
+}
